56dfcb8a5bbaa6f54055f67605e4fb28707a56fa
AntonioRichard <94693266+AntonioRichard@users.noreply.github.com>
2022-04-15 16:50:38 +0300

    Add files via upload

---
+++ TableViewEx/TableViewEx/SupermanViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SupermanViewController.swift
+//  TableViewEx
+//
+//  Created by Antonio - Raul RICHARD on 14.04.2022.
+//
+
+import UIKit
+
+class SupermanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
